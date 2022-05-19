ea9419514eddcbbd5998dae5c3e8803ac71c0c61
esubalew.seife@gmail.com <esubalew.seife@gmail.com>
2022-05-19 10:12:10 -0700

    conveting to context and rendering products list

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import ProductsProvider from "./context/products-context";
+import "./index.css";
+import App from "./App";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
-
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
+// import productReducer from "./store/reducers/products";
+// import { Provider } from 'react-redux';
+// import { combineReducers, createStore } from 'redux';
+// const rootReducer = combineReducers({
+//   shop: productReducer
+// });
+// const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
+  <ProductsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </ProductsProvider>,
+  document.getElementById("root")
 );
